90c2076e56a6375126c157552fbce1324dedbb30
yarahegazy502 <yarahegazy502@gmail.com>
2024-08-09 14:31:07 +0200

    "Refactor HomeComponent: reorganize imports, remove hardcoded data, add new method handleSuccessHomeData, and update chart data"

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,15 @@
 import { ActivitieCardComponent } from './activitie-card/activitie-card.component';
+import { HomeSkeletonComponent } from './home-skeleton/home-skeleton.component';
 import { HomeSearchComponent } from './home-search/home-search.component';
+import { AlertsService } from '../../services/generic/alerts.service';
+import { catchError, finalize, Subscription, tap } from 'rxjs';
+import { HomeService } from '../../services/home.service';
 import { Activities } from '../../interfaces/home';
 import { DropdownModule } from 'primeng/dropdown';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ChartModule } from 'primeng/chart';
 import { Component } from '@angular/core';
-import { catchError, finalize, Subscription, tap } from 'rxjs';
-import { HomeService } from '../../services/home.service';
-import { AlertsService } from '../../services/generic/alerts.service';
-import { HomeSkeletonComponent } from './home-skeleton/home-skeleton.component';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -39,14 +39,9 @@ export class HomeComponent {
   ];
   statusType: any = this.statusTypes[1];
 
-  activities: Activities[] = [
-    { id: 1, count: 1200, total: 100, actionType: new Date() },
-    { id: 2, count: 1432, total: 1200, actionType: new Date() },
-    { id: 3, count: 1765, total: 3200, actionType: new Date() },
-    { id: 4, count: 6543, total: 980, actionType: new Date() },
-  ];
+  activities: Activities[] = [];
 
-  dataItems: any = [6, 4, 2, 1];
+  dataChartItems: any = [];
   data: any;
   chartOptions: any;
 
@@ -58,7 +53,6 @@ export class HomeComponent {
   ngOnInit(): void {
     this.getHomeData();
 
-    this.getChartData();
     this.chartOptions = {
       responsive: true,
       plugins: {
@@ -73,8 +67,9 @@ export class HomeComponent {
     this.isLoadingHomeData = true;
     let homeDataSubscription = this.homeService?.getHomeData()?.pipe(
       tap((res: any) => {
-        if (res?.code === 200) {
+        if (res) {
           this.homeData = res.data;
+          this.handleSuccessHomeData();
         } else {
           this.handleError(res?.message);
         }
@@ -85,6 +80,17 @@ export class HomeComponent {
 
     this.subscriptions.push(homeDataSubscription);
   }
+  handleSuccessHomeData(): void {
+    this.dataChartItems = [6, 4, 2, 1];
+    this.getChartData();
+
+    this.activities = [
+      { id: 1, count: 1200, total: 100, actionType: new Date() },
+      { id: 2, count: 1432, total: 1200, actionType: new Date() },
+      { id: 3, count: 1765, total: 3200, actionType: new Date() },
+      { id: 4, count: 6543, total: 980, actionType: new Date() },
+    ];
+  }
   handleError(err: any): any {
     this.alertsService?.openToast('error', err || 'An error occurred');
   }
@@ -94,7 +100,7 @@ export class HomeComponent {
       labels: ['Total Job', 'In Progress', 'Success', 'Failed'],
       datasets: [
         {
-          data: this.dataItems,
+          data: this.dataChartItems,
           backgroundColor: [
             "#e4e4e4",
             "#005ba4",
@@ -111,7 +117,6 @@ export class HomeComponent {
       ]
     };
   }
-
   getBackgroundColor(index: number): string {
     return this.data.datasets[0].backgroundColor[index];
   }
@@ -119,13 +124,13 @@ export class HomeComponent {
   changeStatusType(): void {
     console.log(this.statusType);
     if (this.statusType?.id == 1) {
-      this.dataItems = [6, 1, 1, 4];
+      this.dataChartItems = [6, 1, 1, 4];
     }
     if (this.statusType?.id == 2) {
-      this.dataItems = [6, 4, 2, 1];
+      this.dataChartItems = [6, 4, 2, 1];
     }
     if (this.statusType?.id == 3) {
-      this.dataItems = [6, 3, 2, 2];
+      this.dataChartItems = [6, 3, 2, 2];
     }
     this.getChartData();
   }
